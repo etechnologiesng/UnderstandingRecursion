ebb6b62536c62213f05525a0199e84a833cbc1a1
Emmanuel Enyinnaya <eenyinnaya@swiftng.net>
2020-07-08 22:06:52 +0100

    Add project files.

---
+++ Algorithms/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using static Algorithms.RecursionFamilyExample;
+
+namespace Algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Child stephen = new Child();           Child Divine = new Child();
+            stephen.sex = "Male";                   Divine.sex = "Male";
+            
+            Child Chidinma = new Child();        Child Child4 = new Child();
+              Chidinma.sex = "Female";             Child4.sex = "Male";
+                       
+            List<Child> EmmanuelsChildren = new List<Child>();
+            EmmanuelsChildren.Add(Divine);
+            EmmanuelsChildren.Add(Chidinma);
+
+            Family EmmanuelsFamily = new Family()
+            {
+                children = EmmanuelsChildren
+            };
+           
+            List<Child> ChidinmaChildren = new List<Child>();
+            ChidinmaChildren.Add(stephen);
+            ChidinmaChildren.Add(Child4);
+
+            Chidinma.family = new Family()
+            {
+                children = ChidinmaChildren
+            };
+
+            int noOfMaleChild = RecursionFamilyExample.CountNumberofMaleChild(EmmanuelsFamily);
+            Console.WriteLine(noOfMaleChild);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Algorithms/Recursion.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms
+{
+    static class  RecursionFamilyExample
+    {
+        public class Family
+        {
+
+            public List<Child> children { get; set; } = new List<Child>();
+        }
+
+        public class Child
+        {
+            public string sex { get; set; }
+            public Family family { get; set; }
+
+        }
+
+        public static int CountNumberofMaleChild(Family family)
+        {
+            //variable to hold the number of male children
+            //base case.
+            if (family.children == null) return 0;
+           
+            int countMale = 0;
+
+            //loop through all children of a family
+            foreach (Child child in family.children)
+            {
+                if(child.sex == "Male")
+                {
+                    countMale++;
+                }
+                //check if the child aslo has a family
+                if (child.family != null)
+                {
+                        countMale += CountNumberofMaleChild(child.family);                  
+                }
+            }
+            return countMale;
+        }
+    }
+}
